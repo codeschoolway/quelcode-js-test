977458540156672287e630c0f3a5f43f76d5a5b1
horik <codeschoolway@gmail.com>
2020-08-16 18:52:05 +0900

    Ajaxを追加

--- js-weather/script.js
+++ js-weather/script.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const INITIAL_CITY = 'London';
-const lang = 'ja';
-const units = 'metric';
-const appid = '4b5774e9f3d2a07b84f0f2f88e486224';
+
 
 var citySelected = document.getElementById("city-select");
 
@@ -12,7 +9,7 @@ document.getElementById("city-select").onchange = function() {
     console.log(citySelected.value); */
     //const city = citySelected.value;
     //var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&lang=${lang}&units=${units}`;
-    getFetch(citySelected.value);
+    ajaxRequest(citySelected.value);
 }
 
 window.onload = function() {
@@ -27,12 +24,47 @@ window.onload = function() {
     //const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&lang=${lang}&units=${units}`;
     //const url = 'aaaa';
     //console.log('aaaaaaaaaa');
-    getFetch(citySelected.value);
+    ajaxRequest(citySelected.value);
 };
 
 
+function ajaxRequest(city) {
+    const lang = 'ja';
+    const units = 'metric';
+    const appid = '4b5774e9f3d2a07b84f0f2f88e486224';
+    //const url = 'https://api.openweathermap.org/data/2.5/forecast';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&lang=${lang}&units=${units}`;
+    //const appId = '保存しておいたAPI Key';
 
-function getFetch(city) {
+    $.ajax(url)
+    .then(function(data){
+        const city = data.name;
+        const date = new Date(toJSTime(data.dt));
+        const myMonth = date.getMonth() + 1;
+        const myDate = date.getDate();
+        const theWeek = ["日", "月", "火", "水", "木", "金", "土"];
+        const numDay = date.getDay();
+        const myWeek = theWeek[numDay];
+        const myHours = date.getHours();
+        const myMin = String(date.getMinutes()).padStart(2, '0');
+        const desc = data.weather[0].description;
+        const temp = Math.round(data.main.temp * 10) / 10;
+        const windspeed = data.wind.speed;
+
+        document.getElementById('city').textContent = city;
+        document.getElementById('date-1').textContent = `${myMonth}月${myDate}日（${myWeek}）`;
+        document.getElementById('date-2').textContent = `${myHours}時${myMin}分`;
+        document.getElementById('desc').textContent = desc;
+        document.getElementById('temp').textContent = temp;
+        document.getElementById('windspeed').textContent = windspeed;
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+
+/* function ajaxRequest(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&lang=${lang}&units=${units}`;
     //console.log(city);
 
@@ -69,7 +101,7 @@ function getFetch(city) {
         console.log(err);
     })
 
-}
+} */
 
 function toJSTime(utcTime) {
     return utcTime * 1000;
